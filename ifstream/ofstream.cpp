3764e548ff1f1fa69117dc9db99e269438e5b683
shiyeong <146803946+shiyeong@users.noreply.github.com>
2023-12-05 22:51:47 +0530

    Update ofstream.cpp

--- ifstream/ofstream.cpp
+++ ifstream/ofstream.cpp
@@ -22,3 +22,41 @@ int main(){
    hout.close();
      return 0;
 }
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ofstream outFile("student_details.txt");
+    int studentId;
+    string studentName;
+
+    cout << "Enter student details:" << endl;
+
+    while (true) {
+        cout << "Student ID (enter -1 to finish): ";
+        cin >> studentId;
+
+        if (studentId == -1) {
+            break;
+        }
+
+        cout << "Student Name: ";
+        cin.ignore();
+        getline(cin, studentName);
+
+        outFile << "Student ID: " << studentId << "\tStudent Name: " << studentName << endl;
+    }
+    outFile.close();
+
+    ifstream inFile("student_details.txt");
+
+    cout << "\nDisplaying student details:" << endl;
+
+    while (getline(inFile, studentName)) {
+        cout << studentName << endl;
+    }
+    inFile.close();
+
+    return 0;
+}
